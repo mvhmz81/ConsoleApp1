5456ac36a2f5280b9dde04730711bdde618ddf23
Mukti Zavery <Mukti.Zavery@ofsted.gov.uk>
2024-06-27 08:36:53 +0100

    more training

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyCodeLibrary;
+using System;
+
+namespace MyClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ScrapeLibrary/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            //WebClient client = new WebClient();
+            //return client.DownloadString(url);
+            ////String reply = client.DownloadString(url);
+            ////Console.WriteLine(reply);
+            return GetWebPage(url);
+
+        }
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            //WebClient client = new WebClient();
+            //string reply = client.DownloadString(url);
+            string reply = GetWebPage(url);
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+        private string GetWebPage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
