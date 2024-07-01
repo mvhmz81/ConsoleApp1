a1aa9166a6e2b41f750a2e9c62914e495cbcaa43
Mukti Zavery <Mukti.Zavery@ofsted.gov.uk>
2024-07-01 11:24:05 +0100

    nlesson wpf

---
+++ HandlingExceptions/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HandlingExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"D:\Users\mzavery\source\repos\ConsoleApp1\HandlingExceptions\Example.txt"); // ensure file is in folder
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine($@"Directory incorrect:   {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        
+    }
+    }
+}
---
+++ TimerExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Timers;
+using System.Threading.Tasks;
+using Timer = System.Timers.Timer; // had to be added as Timer is referenced in two systems
+
+namespace TimerExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the timer
+            Timer myTimer = new Timer(2000); // 2000 milliseconds = 2 seconds
+
+            // Subscribe to the Elapsed event
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.Elapsed += MyTimer_Elapsed1;
+
+            // Start the timer
+            myTimer.Start();
+
+            Console.WriteLine("Press enter to remove the red event");
+            Console.ReadLine();
+
+            myTimer.Elapsed -= MyTimer_Elapsed1;
+            // Keep the program running to listen for timer events
+            Console.ReadLine();
+        }
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            // Output the time of the event
+            Console.WriteLine("Elapsed: {0:HH:mm:ss:fff}", e.SignalTime);
+        }
+        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor= ConsoleColor.Red;
+            // Output the time of the event
+            Console.WriteLine("Elapsed1: {0:HH:mm:ss:fff}", e.SignalTime);
+        }
+    }
+}
